9fc573e0933b54eb35926abd015609b36382ff96
Luka Kalajžić <lukakalajzic@gmail.com>
2023-04-13 11:15:16 +0200

    client folder setup

---
+++ client/app/page.jsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className='text-xs justify-center font-extrabold'>hello</div>
+      <div>fdsfsd</div>
+    </>
+  );
+}
