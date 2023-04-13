0b7a7c6ca4028e1dcd581b7f56a3e45927e5831a
Luka Kalajžić <lukakalajzic@gmail.com>
2023-04-13 11:43:01 +0200

    server  setup

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'eol-last': ['error', 'always'],
+    'indent': ['error', 2, { 'MemberExpression': 1 }],
+    'no-multiple-empty-lines': ['error'],
+    'no-new-symbol': 'error',
+    'no-trailing-spaces': ['error'],
+    'no-undef': ['error'],
+    'no-unused-vars': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    'object-shorthand': 'error',
+    'prefer-const': 2,
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'space-in-parens': ['error', 'never'],
+    'strict': [2, 'never'],
+  },
+};
---
+++ server/mongodb/connect.js
@@ -0,0 +1,11 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = (url) => {
+  // function, accepts url as a param
+  mongoose
+    .connect(url) // connect to db
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+};
+
+export default connectDB;
