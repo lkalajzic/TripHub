8b240d5e3601f62f8bd8adf9b8cb199950cd5059
Luka Kalajžić <lukakalajzic@gmail.com>
2023-04-18 00:01:57 +0200

    cleaned up code

---
+++ client/app/styles/page.jsx
@@ -0,0 +1,28 @@
+import '../../styles/globals.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className='text-heading-hero font-poppins'>heading-hero</div>
+      <div className='text-heading-1'>heading-1</div>
+      <div className='text-heading-2'>heading-2</div>
+      <div className='text-heading-3'>heading-3</div>
+      <div className='text-heading-4'>heading-4</div>
+      <div className='text-body-16'>body-16</div>
+      <div className='text-caption-16'>caption-16</div>
+
+      <button className='bg-primary-blue text-c9 p-5 m-5'>primary-blue</button>
+      <button className='bg-blue-hover text-c9 p-5 m-5'>blue-hover</button>
+      <button className='bg-primary-red text-c9 p-5 m-5'>primary-red</button>
+      <button className='bg-primary-purple text-c9 p-5 m-5'>
+        primary-purple
+      </button>
+      <button className='bg-secondary-orange text-c9 p-5 m-5'>
+        secondary-orange
+      </button>
+      <button className='bg-secondary-sky text-c9 p-5 m-5'>
+        secondary-sky
+      </button>
+    </>
+  );
+}
