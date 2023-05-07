cb8cfbf584cf2349436a0c707e3611a67dac7ded
David Grubešić <128607123+TotalDelay@users.noreply.github.com>
2023-05-07 13:20:55 +0200

    Centered and made work with all screen sizes for components topTour TravePasion and FeaturedDestination

--- client/components/FeaturedDest/FeaturedDest.jsx
+++ client/components/FeaturedDest/FeaturedDest.jsx
@@ -1,35 +1,99 @@
-'use client';
-
-
-import Card from "./Cards";
-import {genRand} from "../../utils"
-import images from "../../public/ImagesFeat/pictures"
+import React from 'react';
+import Image from 'next/image';
+import Card from './Cards';
+import images from '../../public/ImagesFeat/pictures';
 
 const FeaturedDest = () => {
   return (
-    <div class ="max-w-[1170px] mx-auto">
-        <div class="relative w-auto transform top-[0%] bottom-[33.96%]">
-            <h2 class="font-bold h-[82px] font-dm-sans text-[48px] leading tracking-tighter text-c1">
-                Cool Ideas
-            </h2>
-            <div class="w-auto font-normal font-dm-sans text-[16px] leading-[24px] text-c3 flex-shrink-0 order-1 flex-grow-0">
-                If you're out of ideas, maybe consider some of the following options
-            </div>
+    <div className="max-w-[1170px] mx-auto">
+      <div className="relative w-auto transform top-[0%] bottom-[33.96%]">
+        <h2 className="font-bold h-[82px] font-dm-sans text-[48px] leading tracking-tighter text-c1">
+          Cool Ideas
+        </h2>
+        <div className="w-auto font-normal font-dm-sans text-[16px] leading-[24px] text-c3 flex-shrink-0 order-1 flex-grow-0">
+          If you&apos;re out of ideas, maybe consider some of the following options
         </div>
-        <div class="py-[50px] flex gap-[30px] h-[718px] w-[1170px] ">
-            <div class="flex flex-col justify-between items-center h-[718px] w-[870px] gap-[30px] ">
-                <Card img={images[0]} outerWidth="870px" outerHeight="280px" rating={3.6} innerHeight="30px" innerWidth="30px" textSize="40px" smallTextSize="20px" bottomText={`1120 Activities`} topText="Barcelona Tour"/>
-                <div class="flex justify-between w-[870px] h-[408px] gap-[54px] flex-row">
-                    <Card img={images[1]} outerWidth="408px" outerHeight="408px" rating={4.8} innerWidth="30px" innerHeight="19px" textSize="24px" smallTextSize="14px" bottomText={`53 Activities`} topText="London, Big Eye"/>
-                    <Card img={images[2]} outerWidth="408px" outerHeight="408px" rating={3.3} innerWidth="30px" innerHeight="19px" textSize="24px" smallTextSize="14px" bottomText={`33 Activities`} topText="Zimbabwe"/>
-                </div>
-            </div>
-            <div class="flex flex-col justify-between h-[718px] w-[270px] gap-[27px]">
-                <Card img={images[3]} outerWidth="270px" outerHeight="220px" rating={4.9} innerWidth="20px" innerHeight="15px" textSize="18px" smallTextSize="10px" bottomText={`22 Activities`} topText="Soviet Union"/>
-                <Card img={images[4]} outerWidth="270px" outerHeight="220px" rating={3.5} innerWidth="20px" innerHeight="15px" textSize="18px" smallTextSize="10px" bottomText={`21 Activities`} topText="Croatia"/>
-                <Card img={images[5]} outerWidth="270px" outerHeight="220px" rating={2.2} innerWidth="20px" innerHeight="15px" textSize="18px" smallTextSize="10px" bottomText={`14 Activities`} topText="Dubai"/>
-            </div>
+      </div>
+      <div className="py-[50px] flex gap-[30px] h-[718px] w-[1170px] ">
+        <div className="flex flex-col justify-between items-center h-[718px] w-[870px] gap-[30px] ">
+          <Card
+            img={images[0]}
+            outerWidth="870px"
+            outerHeight="280px"
+            rating={3.6}
+            innerHeight="30px"
+            innerWidth="30px"
+            textSize="40px"
+            smallTextSize="20px"
+            bottomText={`1120 Activities`}
+            topText="Barcelona Tour"
+          />
+          <div className="flex justify-between w-[870px] h-[408px] gap-[54px] flex-row">
+            <Card
+              img={images[1]}
+              outerWidth="408px"
+              outerHeight="408px"
+              rating={4.8}
+              innerWidth="30px"
+              innerHeight="19px"
+              textSize="24px"
+              smallTextSize="14px"
+              bottomText={`53 Activities`}
+              topText="London, Big Eye"
+            />
+            <Card
+              img={images[2]}
+              outerWidth="408px"
+              outerHeight="408px"
+              rating={3.3}
+              innerWidth="30px"
+              innerHeight="19px"
+              textSize="24px"
+              smallTextSize="14px"
+              bottomText={`33 Activities`}
+              topText="Zimbabwe"
+            />
+          </div>
         </div>
+        <div className="flex flex-col justify-between h-[718px] w-[270px] gap-[27px]">
+          <Card
+            img={images[3]}
+            outerWidth="270px"
+            outerHeight="220px"
+            rating={4.9}
+            innerWidth="20px"
+            innerHeight="15px"
+            textSize="18px"
+            smallTextSize="10px"
+            bottomText={`22 Activities`}
+            topText="Soviet Union"
+          />
+          <Card
+            img={images[4]}
+            outerWidth="270px"
+            outerHeight="220px"
+            rating={3.5}
+            innerWidth="20px"
+            innerHeight="15px"
+            textSize="18px"
+            smallTextSize="10px"
+            bottomText={`21 Activities`}
+            topText="Croatia"
+          />
+          <Card
+            img={images[5]}
+            outerWidth="270px"
+            outerHeight="220px"
+            rating={2.2}
+            innerWidth="20px"
+            innerHeight="15px"
+            textSize="18px"
+            smallTextSize="10px"
+            bottomText={`14 Activities`}
+            topText="Dubai"
+          />   
+        </div>
+      </div>
     </div>
     
     
--- client/components/TravelPasion/TravelPasion.jsx
+++ client/components/TravelPasion/TravelPasion.jsx
@@ -1,42 +1,37 @@
-'use client';
-
 import cardList from '../../constants/TravelCards';
-import Card from "./Cards";
-
+import Card from './Cards';
 
 const TravelPasion = () => {
-    return (
-        <div class ="max-w-[1170px]  md:max-w-[585px] sm:max-w-[270px] mx-auto lg:h-[560px] md:h-[900px] sm:h-[1600px]">
-            <div class="relative  w-[1014px] transform top-[0%] bottom-[30px]">
-                <h2 class="font-bold h-[82px] font-dm-sans text-[48px] leading tracking-tighter text-c1">
-                    Travel Your Passion
-                </h2>
-                <div class="w-[1014px] text-[#84878B] font-normal font-dm-sans text-[16px] leading-[24px] flex-shrink-0 order-1 flex-grow-0">
-                    Most Brilliant reasons TripHub should be your one-stop-shop!
-                </div>
-            </div>
-            
-            <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-[30px] absolute py-[20px] ">  
-                        {cardList.map((cardInfo) => (
-                            <Card 
-                                outerWidth="270px" 
-                                outerHeight="324px" 
-                                innerWidth="30px" 
-                                innerHeight="30px" 
-                                textSize="34px" 
-                                smallTextSize="16px" 
-                                topText={cardInfo.title}
-                                bottomText={cardInfo.text}
-                                city={cardInfo.city} 
-                                img={cardInfo.img}/>
-                            
-                        ))}    
-                
-
-
-            </div>
+  return (
+    <div className="mx-auto lg:max-w-[1170px] md:max-w-[585px] sm:max-w-[270px] lg:h-[560px] md:h-[900px] sm:h-[1600px]">
+      <div className="relative w-[1014px] transform top-[0%] bottom-[30px]">
+        <h2 className="font-bold h-[82px] font-dm-sans text-[48px] leading tracking-tighter text-c1">
+          Travel Your Passion
+        </h2>
+        <div className="w-[1014px] font-normal font-dm-sans text-[16px] leading-[24px] text-[#84878B] flex-shrink-0 order-1 flex-grow-0">
+          Most Brilliant reasons TripHub should be your one-stop-shop!
         </div>
-    )
+      </div>
+
+      <div className="grid gap-[30px] grid-cols-1 md:grid-cols-2 lg:grid-cols-4 absolute py-[20px]">
+        {cardList.map(({ title, text, city, img }) => (
+          <Card
+            key={title}
+            outerWidth="270px"
+            outerHeight="324px"
+            innerWidth="30px"
+            innerHeight="30px"
+            textSize="34px"
+            smallTextSize="16px"
+            topText={title}
+            bottomText={text}
+            city={city}
+            img={img}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TravelPasion;
