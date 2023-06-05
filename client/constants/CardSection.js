f356a6f1fa5a7d3d5e0c4ede26f1e23aa1f88e67
Luka Antunovic <luka.antunovic6@gmail.com>
2023-06-06 01:09:53 +0200

    Resised and reformated pictures Based on the Code profiling excercise Fixed FeaturedDestinations error message popup

--- client/constants/CardSection.js
+++ client/constants/CardSection.js
@@ -1,41 +1,41 @@
 export default [
   {
-    img: '/images/Paris.jpg',
+    img: '/images/Paris.webp',
     title: 'Paris, France',
     text: 'See 38 destinations',
   },
   {
-    img: '/images/Amsterdam.jpg',
+    img: '/images/Amsterdam.webp',
     title: 'Amsterdam, Netherlands',
     text: 'See 27 destinations',
   },
   {
-    img: '/images/London.jpg',
+    img: '/images/London.webp',
     title: 'London, England',
     text: 'See 46 destinations',
   },
   {
-    img: '/images/Rome.jpg',
+    img: '/images/Rome.webp',
     title: 'Rome, Italy',
     text: 'See 23 destinations',
   },
   {
-    img: '/images/Cairo.jpg',
+    img: '/images/Cairo.webp',
     title: 'Cairo, Egypt',
     text: 'See 32 destinations',
   },
   {
-    img: '/images/Athens.jpg',
+    img: '/images/Athens.webp',
     title: 'Athens, Greece',
     text: 'See 28 destinations',
   },
   {
-    img: '/images/Dubai.jpg',
+    img: '/images/Dubai.webp',
     title: 'Dubai, UAE',
     text: 'See 24 destinations',
   },
   {
-    img: '/images/Tokyo.jpg',
+    img: '/images/Tokyo.webp',
     title: 'Tokyo, Japan',
     text: 'See 39 destinations',
   },
