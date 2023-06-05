a06fabd6927c0579bc294e233b9546f872262cd0
YSL-Slime <lulicrok2@gmail.com>
2023-06-05 20:08:54 +0200

    Add new feature

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -8,11 +8,13 @@ import FeaturedDestinations from '../components/FeaturedDestinations/FeaturedDes
 import TopTour from '../components/TopTour/TopTour';
 import ExploreWorld from '../components/Hotel/ExploreWorld';
 import TrendingCites from '../components/Hotel/TrendingCites';
+import Login from '../components/Signig/Login';
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Login />
       <SearchField />
       <CardSection />
       <FeaturedDestinations />
