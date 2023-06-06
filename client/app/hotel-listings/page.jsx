17318692bab4ff97031d06aab11fa3c844e75f57
Luka Kalajžić <112662366+lkalajzic@users.noreply.github.com>
2023-06-07 00:17:43 +0200

    finished HotelListings page, added filtering by hotel type, price filtering, and completed the responsiveness of the page

--- client/app/hotel-listings/page.jsx
+++ client/app/hotel-listings/page.jsx
@@ -1,19 +1,62 @@
+'use client';
+import React, { useState } from 'react';
+import Link from 'next/link';
 import '../../styles/globals.css';
 import Navbar from '../../components/Navbar';
-import SearchField from '../../components/SearchField';
+import SearchField2 from '../../components/SearchField2';
 import Footer from '../../components/Footer';
 import HotelList from '../../components/HotelListings/HotelList';
 import Filter from '../../components/HotelListings/Filter';
 
 export default function Home() {
+  const [isFilterVisible, setFilterVisible] = useState(false);
+
+  const handleDivClick = () => {
+    setFilterVisible(!isFilterVisible);
+  };
+
   return (
     <>
       <Navbar />
       <div className='bg-[#fafafb]'>
-        <SearchField />
-        <div className='flex flex-row justify-center items-start m-5 gap-[40px]'>
-          <Filter />
+        <div className='flex justify-between items-center'>
+          <div className='flex flex-row items-center p-10'>
+            <Link href='/'>
+              <p className=''>Home &gt;</p>
+            </Link>
+            <p className='text-c4 pl-2 text-caption-14'>Hotel listings</p>
+          </div>
+          <div className='pr-10 md:hidden' onClick={handleDivClick}>
+            <svg
+              width='28'
+              height='28'
+              viewBox='0 0 24 24'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M14.6995 12.8105V17.3324C14.6995 17.669 14.5069 17.9768 14.2021 18.1274L10.6028 19.905C10.0045 20.2005 9.30051 19.7708 9.30051 19.11V12.8105L3.20511 5.45166C2.7244 4.87132 3.14242 4 3.90154 4H20.0985C20.8576 4 21.2756 4.87132 20.7949 5.45166L14.6995 12.8105ZM12.8998 16.7813V12.4914C12.8998 12.2863 12.9717 12.0874 13.1032 11.9286L18.1996 5.77588H5.80036L10.8967 11.9286C11.0283 12.0874 11.1001 12.2863 11.1001 12.4914V17.6701L12.8998 16.7813Z'
+                fill='#B1B5C4'
+              />
+            </svg>
+          </div>
+        </div>
+
+        <div className='hidden lg:flex'>
+          <SearchField2 />
+        </div>
+        <div className='flex flex-col md:flex-row justify-center m-10 md:mx-20 gap-[40px]'>
+          <div className={`md:flex ${isFilterVisible ? '' : 'hidden'}`}>
+            <Filter />
+          </div>
           <HotelList />
+        </div>
+        <div className='flex items-center justify-center p-5 pb-8'>
+          <button className='text-c1 text-caption-16 w-[176px] h-[46px] rounded-[30px] border-[1px] py-[11px] px-[41px] hover:border-primary-blue'>
+            View more
+          </button>
         </div>
       </div>
       <Footer />
--- client/components/HotelListings/Filter.jsx
+++ client/components/HotelListings/Filter.jsx
@@ -1,137 +1,187 @@
+'use client';
+import React, { useState } from 'react';
+
 const Filter = () => {
+  const [value, setValue] = useState(100);
+
+  const handleSliderChange = (event) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+  };
+
   return (
     <>
-      <div className='hidden md:block'>
-        <div className='flex flex-col justify-betweem items-start gap-5 w-[290px] h-[1322px]'>
-          <div className='flex flex-col gap-[30px]'>
-            <div className='flex flex-col items-start gap-[14px]'>
-              <p className='text-sidebar text-body-18'>
-                Search location or property
-              </p>
-              <div className='flex items-center gap-2 bg-white rounded-[10px] px-[11px] py-[14px] border hover:border-primary-blue'>
-                <input
-                  type='text'
-                  placeholder='Search location or property'
-                  className='focus:outline-none bg-transparent text-caption-16 w-[204px]'
-                />
-                <img src='../../hotels/icons/Search.svg' />
-              </div>
+      <div className='flex flex-col mx-auto justify-betweem items-start gap-5 w-[290px] h-[525px]'>
+        <div className='flex flex-col gap-[30px]'>
+          <div className='flex flex-col items-start gap-[14px]'>
+            <p className='text-sidebar text-body-18'>
+              Search location or property
+            </p>
+            <div className='flex items-center gap-2 bg-white rounded-[10px] px-[11px] py-[14px] border hover:border-primary-blue'>
+              <input
+                type='text'
+                placeholder='Search location or property'
+                className='focus:outline-none bg-transparent text-caption-16 w-[204px]'
+              />
+              <img src='../../hotels/icons/Search.svg' />
             </div>
-            <div className='flex flex-col justify-between items-start gap-[15px]'>
-              <p className='text-c1 text-body-18'>Property type</p>
-              <div className='flex flex-row justify-between items-center gap-3'>
-                <input
-                  type='checkbox'
-                  id='hotel-checkbox'
-                  className='false cursor-pointer mr-3 appearance-none h-[22px] w-[22px] rounded-[6px] border-[1.5px] border-c4 hover:border-primary-blue checked:bg-primary-blue'
-                />
-                <label
-                  htmlFor='hotel-checkbox'
-                  className='text-checkbox-14 cursor-pointer'
-                >
-                  Hotel
-                </label>
-                <svg
-                  width='12'
-                  height='9'
-                  viewBox='0 0 12 9'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='absolute left-[223px] pointer-events-none'
-                >
-                  <path
-                    d='M11.7251 0.419985C11.6457 0.346628 11.5513 0.288403 11.4472 0.248668C11.3432 0.208934 11.2316 0.188477 11.1189 0.188477C11.0062 0.188477 10.8946 0.208934 10.7905 0.248668C10.6865 0.288403 10.592 0.346628 10.5126 0.419985L4.15139 6.2586L1.47882 3.80106C1.3964 3.72809 1.29911 3.67071 1.1925 3.6322C1.08589 3.59369 0.972054 3.5748 0.857483 3.57662C0.742911 3.57844 0.629852 3.60092 0.524761 3.64279C0.419669 3.68465 0.324603 3.74509 0.244991 3.82063C0.165378 3.89617 0.102779 3.98535 0.0607656 4.08307C0.0187524 4.18079 -0.00185163 4.28513 0.000130571 4.39015C0.00211277 4.49517 0.0266423 4.5988 0.0723181 4.69513C0.117994 4.79145 0.183922 4.87859 0.266338 4.95157L3.54515 7.95697C3.62453 8.03033 3.71897 8.08855 3.82302 8.12829C3.92707 8.16802 4.03867 8.18848 4.15139 8.18848C4.26411 8.18848 4.37572 8.16802 4.47977 8.12829C4.58382 8.08855 4.67826 8.03033 4.75763 7.95697L11.7251 1.57049C11.8118 1.4972 11.881 1.40825 11.9283 1.30924C11.9756 1.21024 12 1.10332 12 0.995238C12 0.887152 11.9756 0.780237 11.9283 0.681231C11.881 0.582225 11.8118 0.493275 11.7251 0.419985Z'
-                    fill='white'
-                  />
-                </svg>
-              </div>
-              <div className='flex flex-row justify-between items-center gap-3'>
-                <input
-                  type='checkbox'
-                  id='apartment-checkbox'
-                  className='false cursor-pointer mr-3 appearance-none h-[22px] w-[22px] rounded-[6px] border-[1.5px] border-c4 hover:border-primary-blue checked:bg-primary-blue'
-                />
-                <label
-                  htmlFor='apartment-checkbox'
-                  className='text-checkbox-14 cursor-pointer'
-                >
-                  Apartment
-                </label>
-                <svg
-                  width='12'
-                  height='9'
-                  viewBox='0 0 12 9'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='absolute left-[223px] pointer-events-none'
-                >
-                  <path
-                    d='M11.7251 0.419985C11.6457 0.346628 11.5513 0.288403 11.4472 0.248668C11.3432 0.208934 11.2316 0.188477 11.1189 0.188477C11.0062 0.188477 10.8946 0.208934 10.7905 0.248668C10.6865 0.288403 10.592 0.346628 10.5126 0.419985L4.15139 6.2586L1.47882 3.80106C1.3964 3.72809 1.29911 3.67071 1.1925 3.6322C1.08589 3.59369 0.972054 3.5748 0.857483 3.57662C0.742911 3.57844 0.629852 3.60092 0.524761 3.64279C0.419669 3.68465 0.324603 3.74509 0.244991 3.82063C0.165378 3.89617 0.102779 3.98535 0.0607656 4.08307C0.0187524 4.18079 -0.00185163 4.28513 0.000130571 4.39015C0.00211277 4.49517 0.0266423 4.5988 0.0723181 4.69513C0.117994 4.79145 0.183922 4.87859 0.266338 4.95157L3.54515 7.95697C3.62453 8.03033 3.71897 8.08855 3.82302 8.12829C3.92707 8.16802 4.03867 8.18848 4.15139 8.18848C4.26411 8.18848 4.37572 8.16802 4.47977 8.12829C4.58382 8.08855 4.67826 8.03033 4.75763 7.95697L11.7251 1.57049C11.8118 1.4972 11.881 1.40825 11.9283 1.30924C11.9756 1.21024 12 1.10332 12 0.995238C12 0.887152 11.9756 0.780237 11.9283 0.681231C11.881 0.582225 11.8118 0.493275 11.7251 0.419985Z'
-                    fill='white'
-                  />
-                </svg>
-              </div>
-              <div className='flex flex-row justify-between items-center gap-3'>
-                <input
-                  type='checkbox'
-                  id='resort-checkbox'
-                  className='false cursor-pointer mr-3 appearance-none h-[22px] w-[22px] rounded-[6px] border-[1.5px] border-c4 hover:border-primary-blue checked:bg-primary-blue'
-                />
-                <label
-                  htmlFor='resort-checkbox'
-                  className='text-checkbox-14 cursor-pointer'
-                >
-                  Resort
-                </label>
-                <svg
-                  width='12'
-                  height='9'
-                  viewBox='0 0 12 9'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
-                  className='absolute left-[223px] pointer-events-none'
-                >
-                  <path
-                    d='M11.7251 0.419985C11.6457 0.346628 11.5513 0.288403 11.4472 0.248668C11.3432 0.208934 11.2316 0.188477 11.1189 0.188477C11.0062 0.188477 10.8946 0.208934 10.7905 0.248668C10.6865 0.288403 10.592 0.346628 10.5126 0.419985L4.15139 6.2586L1.47882 3.80106C1.3964 3.72809 1.29911 3.67071 1.1925 3.6322C1.08589 3.59369 0.972054 3.5748 0.857483 3.57662C0.742911 3.57844 0.629852 3.60092 0.524761 3.64279C0.419669 3.68465 0.324603 3.74509 0.244991 3.82063C0.165378 3.89617 0.102779 3.98535 0.0607656 4.08307C0.0187524 4.18079 -0.00185163 4.28513 0.000130571 4.39015C0.00211277 4.49517 0.0266423 4.5988 0.0723181 4.69513C0.117994 4.79145 0.183922 4.87859 0.266338 4.95157L3.54515 7.95697C3.62453 8.03033 3.71897 8.08855 3.82302 8.12829C3.92707 8.16802 4.03867 8.18848 4.15139 8.18848C4.26411 8.18848 4.37572 8.16802 4.47977 8.12829C4.58382 8.08855 4.67826 8.03033 4.75763 7.95697L11.7251 1.57049C11.8118 1.4972 11.881 1.40825 11.9283 1.30924C11.9756 1.21024 12 1.10332 12 0.995238C12 0.887152 11.9756 0.780237 11.9283 0.681231C11.881 0.582225 11.8118 0.493275 11.7251 0.419985Z'
-                    fill='white'
-                  />
-                </svg>
-              </div>
+          </div>
+          <svg
+            width='277'
+            height='1'
+            viewBox='0 0 277 1'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <rect width='277' height='1' fill='#E7ECF3' />
+          </svg>
+          <div className='flex flex-col justify-between items-start gap-[15px]'>
+            <p className='text-c1 text-body-18'>Property type</p>
+            <div className='relative flex flex-row justify-between items-center gap-3'>
+              <input
+                type='checkbox'
+                id='hotel-checkbox'
+                className='false cursor-pointer mr-3 appearance-none h-[22px] w-[22px] rounded-[6px] border-[1.5px] border-c4 hover:border-primary-blue checked:bg-primary-blue'
+              />
+              <label
+                htmlFor='hotel-checkbox'
+                className='text-checkbox-14 cursor-pointer'
+              >
+                Hotel
+              </label>
               <svg
-                width='277'
-                height='1'
-                viewBox='0 0 277 1'
+                width='12'
+                height='9'
+                viewBox='0 0 12 9'
                 fill='none'
                 xmlns='http://www.w3.org/2000/svg'
+                className='absolute left-[5px] pointer-events-none'
               >
-                <rect width='277' height='1' fill='#E7ECF3' />
+                <path
+                  d='M11.7251 0.419985C11.6457 0.346628 11.5513 0.288403 11.4472 0.248668C11.3432 0.208934 11.2316 0.188477 11.1189 0.188477C11.0062 0.188477 10.8946 0.208934 10.7905 0.248668C10.6865 0.288403 10.592 0.346628 10.5126 0.419985L4.15139 6.2586L1.47882 3.80106C1.3964 3.72809 1.29911 3.67071 1.1925 3.6322C1.08589 3.59369 0.972054 3.5748 0.857483 3.57662C0.742911 3.57844 0.629852 3.60092 0.524761 3.64279C0.419669 3.68465 0.324603 3.74509 0.244991 3.82063C0.165378 3.89617 0.102779 3.98535 0.0607656 4.08307C0.0187524 4.18079 -0.00185163 4.28513 0.000130571 4.39015C0.00211277 4.49517 0.0266423 4.5988 0.0723181 4.69513C0.117994 4.79145 0.183922 4.87859 0.266338 4.95157L3.54515 7.95697C3.62453 8.03033 3.71897 8.08855 3.82302 8.12829C3.92707 8.16802 4.03867 8.18848 4.15139 8.18848C4.26411 8.18848 4.37572 8.16802 4.47977 8.12829C4.58382 8.08855 4.67826 8.03033 4.75763 7.95697L11.7251 1.57049C11.8118 1.4972 11.881 1.40825 11.9283 1.30924C11.9756 1.21024 12 1.10332 12 0.995238C12 0.887152 11.9756 0.780237 11.9283 0.681231C11.881 0.582225 11.8118 0.493275 11.7251 0.419985Z'
+                  fill='white'
+                />
               </svg>
             </div>
+            <div className='relative flex flex-row justify-between items-center gap-3'>
+              <input
+                type='checkbox'
+                id='apartment-checkbox'
+                className='false cursor-pointer mr-3 appearance-none h-[22px] w-[22px] rounded-[6px] border-[1.5px] border-c4 hover:border-primary-blue checked:bg-primary-blue'
+              />
+              <label
+                htmlFor='apartment-checkbox'
+                className='text-checkbox-14 cursor-pointer'
+              >
+                Apartment
+              </label>
+              <svg
+                width='12'
+                height='9'
+                viewBox='0 0 12 9'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+                className='absolute left-[5px] pointer-events-none'
+              >
+                <path
+                  d='M11.7251 0.419985C11.6457 0.346628 11.5513 0.288403 11.4472 0.248668C11.3432 0.208934 11.2316 0.188477 11.1189 0.188477C11.0062 0.188477 10.8946 0.208934 10.7905 0.248668C10.6865 0.288403 10.592 0.346628 10.5126 0.419985L4.15139 6.2586L1.47882 3.80106C1.3964 3.72809 1.29911 3.67071 1.1925 3.6322C1.08589 3.59369 0.972054 3.5748 0.857483 3.57662C0.742911 3.57844 0.629852 3.60092 0.524761 3.64279C0.419669 3.68465 0.324603 3.74509 0.244991 3.82063C0.165378 3.89617 0.102779 3.98535 0.0607656 4.08307C0.0187524 4.18079 -0.00185163 4.28513 0.000130571 4.39015C0.00211277 4.49517 0.0266423 4.5988 0.0723181 4.69513C0.117994 4.79145 0.183922 4.87859 0.266338 4.95157L3.54515 7.95697C3.62453 8.03033 3.71897 8.08855 3.82302 8.12829C3.92707 8.16802 4.03867 8.18848 4.15139 8.18848C4.26411 8.18848 4.37572 8.16802 4.47977 8.12829C4.58382 8.08855 4.67826 8.03033 4.75763 7.95697L11.7251 1.57049C11.8118 1.4972 11.881 1.40825 11.9283 1.30924C11.9756 1.21024 12 1.10332 12 0.995238C12 0.887152 11.9756 0.780237 11.9283 0.681231C11.881 0.582225 11.8118 0.493275 11.7251 0.419985Z'
+                  fill='white'
+                />
+              </svg>
+            </div>
+            <div className='relative flex flex-row justify-between items-center gap-3'>
+              <input
+                type='checkbox'
+                id='resort-checkbox'
+                className='false cursor-pointer mr-3 appearance-none h-[22px] w-[22px] rounded-[6px] border-[1.5px] border-c4 hover:border-primary-blue checked:bg-primary-blue'
+              />
+              <label
+                htmlFor='resort-checkbox'
+                className='text-checkbox-14 cursor-pointer'
+              >
+                Resort
+              </label>
+              <svg
+                width='12'
+                height='9'
+                viewBox='0 0 12 9'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+                className='absolute left-[5px] pointer-events-none'
+              >
+                <path
+                  d='M11.7251 0.419985C11.6457 0.346628 11.5513 0.288403 11.4472 0.248668C11.3432 0.208934 11.2316 0.188477 11.1189 0.188477C11.0062 0.188477 10.8946 0.208934 10.7905 0.248668C10.6865 0.288403 10.592 0.346628 10.5126 0.419985L4.15139 6.2586L1.47882 3.80106C1.3964 3.72809 1.29911 3.67071 1.1925 3.6322C1.08589 3.59369 0.972054 3.5748 0.857483 3.57662C0.742911 3.57844 0.629852 3.60092 0.524761 3.64279C0.419669 3.68465 0.324603 3.74509 0.244991 3.82063C0.165378 3.89617 0.102779 3.98535 0.0607656 4.08307C0.0187524 4.18079 -0.00185163 4.28513 0.000130571 4.39015C0.00211277 4.49517 0.0266423 4.5988 0.0723181 4.69513C0.117994 4.79145 0.183922 4.87859 0.266338 4.95157L3.54515 7.95697C3.62453 8.03033 3.71897 8.08855 3.82302 8.12829C3.92707 8.16802 4.03867 8.18848 4.15139 8.18848C4.26411 8.18848 4.37572 8.16802 4.47977 8.12829C4.58382 8.08855 4.67826 8.03033 4.75763 7.95697L11.7251 1.57049C11.8118 1.4972 11.881 1.40825 11.9283 1.30924C11.9756 1.21024 12 1.10332 12 0.995238C12 0.887152 11.9756 0.780237 11.9283 0.681231C11.881 0.582225 11.8118 0.493275 11.7251 0.419985Z'
+                  fill='white'
+                />
+              </svg>
+            </div>
+            <svg
+              width='277'
+              height='1'
+              viewBox='0 0 277 1'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <rect width='277' height='1' fill='#E7ECF3' />
+            </svg>
+          </div>
+
+          <div>
             <div>
               <label
-                for='price-range'
-                className='text-sidebar text-body-18 block mb-2'
+                htmlFor='price-range'
+                className='text-sidebar text-body-18'
               >
                 Price range
               </label>
-              <input
-                id='price-range'
-                type='range'
-                value='100'
-                min='50'
-                max='200'
-                class='w-full h-2 bg-primary-blue rounded-lg cursor-pointer'
-              />
-              <div className='flex justify-between'>
-                <span className='text-c3 text-caption-16 '>€50</span>
-                <span className='text-c3 text-caption-16 '>€200</span>
+              <div className='flex justify-between  mt-10'>
+                <div className='relative w-3/4'>
+                  <input
+                    id='price-range'
+                    type='range'
+                    value={value}
+                    min='50'
+                    max='200'
+                    step='1'
+                    onChange={handleSliderChange}
+                    className='relative w-full h-2 bg-primary-blue rounded-lg cursor-pointer'
+                  />
+                  <div
+                    className='absolute pointer-events-none	 w-[80px] h-[66px] bottom-[28px] left-[50%] transform translate-x-[-50%]'
+                    style={{ left: `${((value - 50) / 150) * 100}%` }}
+                  >
+                    <img
+                      src='../../hotels/icons/slider.png'
+                      className='w-full h-full'
+                    />
+                    <span className='text-white absolute left-1/2 top-[24px] transform -translate-x-1/2 -translate-y-1/2'>
+                      {value}
+                    </span>
+                  </div>
+                  <div className='flex justify-between pt-2'>
+                    <span className='text-c3 text-caption-16'>€50</span>
+                    <span className='text-c3 text-caption-16'>€200</span>
+                  </div>
+                </div>
+
+                <input
+                  type='number'
+                  value={value}
+                  min='50'
+                  max='200'
+                  step='1'
+                  onChange={handleInputChange}
+                  className='ml-2 pl-3 py-1  w-[64px] h-[30px] text-body-16 bg-white border border-primary-blue rounded'
+                />
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className='hidden md:block'>
-        <div className='flex flex-col items-center'></div>
       </div>
     </>
   );
