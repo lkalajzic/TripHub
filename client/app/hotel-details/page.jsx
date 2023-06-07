a6f570511e87da993d467853c84f0cd3fd2a101a
Luka Kalajžić <112662366+lkalajzic@users.noreply.github.com>
2023-06-07 15:42:28 +0200

    fixes, code integration

--- client/app/hotel-details/page.jsx
+++ client/app/hotel-details/page.jsx
@@ -1,15 +1,13 @@
-'use client';
-
-import React, { useState } from 'react';
-import Link from 'next/link';
 import '../../styles/globals.css';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import HotelDetail from '../../components/HotelDetail/HotelDetail';
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <HotelDetail />
       <Footer />
     </>
   );
