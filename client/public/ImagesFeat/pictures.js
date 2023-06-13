c64ecac5749d15b984f6f97bd6e4a2db11e4a01b
David Grubešić <128607123+TotalDelay@users.noreply.github.com>
2023-06-13 09:44:50 +0200

    FIxed pictures resolutions

--- client/public/ImagesFeat/pictures.js
+++ client/public/ImagesFeat/pictures.js
@@ -1,7 +1,7 @@
 import Image1 from './barcelona.webp';
 import Image6 from "./Dubai.webp";
 import Image4 from "./soviet.jpg";
-import Image5 from "./cro.jpg";
+import Image5 from "./Croatia.jpg";
 import Image2 from "./London2.jpg";
 import Image3 from "./Zimb.jpg";
 
